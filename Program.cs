8fa320ad23c0f1b5e96d3f9025c4a7d9a50b751d
Douglas Saldaña De la Rosa <douglas.delarosa@gmail.com>
2018-06-07 16:06:23 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pratica_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
